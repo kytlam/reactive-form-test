84854357a73004cbb7b78b5efbc7e4a908757bd2
terrylam <terrylam@gmail.com>
2021-12-05 02:05:44 +0800

    update

--- src/app/modules/order-form/component/dynamic-form/dynamic-form.component.ts
+++ src/app/modules/order-form/component/dynamic-form/dynamic-form.component.ts
@@ -1,19 +1,127 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormArray, FormBuilder, FormControl, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { SyncValidator } from '../../validators/custom-async-validator';
 
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
-  styleUrls: ['./dynamic-form.component.css']
+  styleUrls: ['./dynamic-form.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DynamicFormComponent),
+      multi: true
+    }
+  ]
 })
-export class DynamicFormComponent implements OnInit {
-  form!: FormGroup;
-  formFields!: any[];
+export class DynamicFormComponent implements ControlValueAccessor, OnInit, OnDestroy {
 
-  constructor() { }
+  _form!: FormGroup;
+  @Input() jsonFormSchema!: any[];
 
-  ngOnInit() {
-    
+  private _destroy$: Subject<void> = new Subject<void>();
+
+  public onChange: (value: string) => void;
+  public onTouched: () => void;
+
+  constructor(private fb: FormBuilder, private changeDetectorRef: ChangeDetectorRef) { }
+
+  public writeValue(value: any): void {
+    if (value && value === "") {
+      return;
+    }
+
+    if (value) {
+      this._form.patchValue(value);
+    }
   }
 
+  public registerOnChange(fn: (value: any) => void): void {
+    // this._form.valueChanges.subscribe(values => fn(values));
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(_isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnInit() {
+    if(this.jsonFormSchema && this.jsonFormSchema.length > 0) {
+      this.buildForm();
+    }
+  }
+
+  buildForm() {
+    this._createFormGroup();
+    this._setupObservables();
+  }
+
+  _createFormGroup() {
+    // this._form = this.fb.group({});
+    this._form = this._createGroup();
+    // Object.keys(this.jsonFormSchema).forEach((key:string) => {
+    // this.jsonFormSchema.forEach((key:string) => {
+    //   let group = this._createGroup(key);
+    //   this._form.addControl(key, group);
+    // });
+    console.log("xxxxx" , this.jsonFormSchema);
+    console.log("xxxxx" , this._form);
+  }
+
+  _createGroup = (): FormGroup => {
+    const group = this.fb.group({});
+    this.jsonFormSchema.filter(({controlType}) => controlType !== "button")
+      .forEach((element: any) => {
+      group.addControl(element.name, this._createControl(element) )
+    });
+    return group
+  }
+
+  _createControl = (config: any): FormControl | FormArray  => {
+    const { disabled, validation, value } = config;
+    // let validator = AsyncValidator();
+    // return this.fb.control({ value: value, disabled: disabled } ,null, validator);
+    let validator = SyncValidator();
+    return this.fb.control({ value: value, disabled: disabled } , validator);
+  }
+  ngOnDestroy() {
+    if (this._destroy$ && !this._destroy$.closed) {
+      this._destroy$.next();
+      this._destroy$.complete();
+    }
+  }
+
+  isValid(name: string ): any {
+    console.log(" isValid: ", name)
+    let control = null;
+    if (this._form) {
+      control= this._form.get(name) as FormControl;
+    }
+    if (control) {
+      console.log(" isValid: ", control)
+      return control.valid || !control.touched;
+    } else {
+      return true;
+    }
+  }
+
+  public onInputBlurred(): void {
+    this.onTouched();
+  }
+  private _setupObservables = (): void => {
+    this._form.valueChanges.pipe(takeUntil(this._destroy$)).subscribe(value => {
+      console.log(" value  changed ", value);
+      this.onChange(value);
+      this.changeDetectorRef.detectChanges;
+      // if (this._onChange) {
+      //   this._onChange(value);
+      // }
+    });
+  }
 }
--- src/app/modules/order-form/page/root-form/root-form.component.ts
+++ src/app/modules/order-form/page/root-form/root-form.component.ts
@@ -13,10 +13,11 @@ import { Subject } from 'rxjs';
   styleUrls: ['./root-form.component.css'],
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RootFormComponent implements OnInit,OnDestroy {
+export class RootFormComponent implements OnInit, OnDestroy {
   rootForm: FormGroup;
-  jsonFormSchema: any[];
-  get FormSchemaKey():string[] { return Object.keys(this.jsonFormSchema); }
+  jsonRootFormSchema: any;
+  get FormSchemaKey():string[] { return Object.keys(this.jsonRootFormSchema); }
+  SubFormSchema(key: string):any[] { return this.jsonRootFormSchema[key] }
   private _destroy$: Subject<void> = new Subject<void>();
 
   constructor(private fb: FormBuilder, private changeDetectorRef: ChangeDetectorRef
@@ -24,7 +25,7 @@ export class RootFormComponent implements OnInit,OnDestroy {
 
   ngOnInit(): void {
     this.jsonCall.getJsonFile("json-forms/test-form").subscribe(res => {
-      this.jsonFormSchema = res.schema;
+      this.jsonRootFormSchema = res.schema;
       this.buildForm();
     })
   }
@@ -36,52 +37,54 @@ export class RootFormComponent implements OnInit,OnDestroy {
 
   _createFormGroup() {
     this.rootForm = this.fb.group({});
-    Object.keys(this.jsonFormSchema).forEach((key:string) => {
-      let group = this._createGroup(key);
-      this.rootForm.addControl(key, group);
+    Object.keys(this.jsonRootFormSchema).forEach((key:string) => {
+      // let group = this._createGroup(key);
+      // this.rootForm.addControl(key, group);
+      this.rootForm.addControl(key, this.fb.control({}))
     });
-    console.log("xxxxx" , this.jsonFormSchema);
+    console.log("xxxxx" , this.jsonRootFormSchema);
     // this.rootForm.addControl('numberbox', this.fb.control({ disabled: false, value: null }, null, [AsyncValidator()]));
     // this.rootForm.addControl('textbox', this.fb.control({ disabled: false, value: null }, null, [AsyncValidator()]));
   }
 
-  _createGroup = (key:string): FormGroup => {
-    const group = this.fb.group({});
-    this.jsonFormSchema[key].filter(({controlType}) => controlType !== "button")
-      .forEach((element: any) => {
-      group.addControl(element.name, this._createControl(element) )
-    });
-    return group
-  }
+  // _createGroup = (key:string): FormGroup => {
+  //   const group = this.fb.group({});
+  //   this.jsonFormSchema[key].filter(({controlType}) => controlType !== "button")
+  //     .forEach((element: any) => {
+  //     group.addControl(element.name, this._createControl(element) )
+  //   });
+  //   return group
+  // }
 
-  _createControl = (config: any):FormControl | FormArray  => {
-    const { disabled, validation, value } = config;
-    // let validator = AsyncValidator();
-    // return this.fb.control({ value: value, disabled: disabled } ,null, validator);
-    let validator = SyncValidator();
-    return this.fb.control({ value: value, disabled: disabled } , validator);
-  }
+  // _createControl = (config: any):FormControl | FormArray  => {
+  //   const { disabled, validation, value } = config;
+  //   // let validator = AsyncValidator();
+  //   // return this.fb.control({ value: value, disabled: disabled } ,null, validator);
+  //   let validator = SyncValidator();
+  //   return this.fb.control({ value: value, disabled: disabled } , validator);
+  // }
   ngOnDestroy() {
     if (this._destroy$ && !this._destroy$.closed) {
       this._destroy$.next();
       this._destroy$.complete();
     }
   }
-  isValid(groupKey: string, name: string ): any {
-    let control = null;
-    if (this.rootForm) {
-      let subControl = this.rootForm.get(groupKey) as FormControl;
-      if (subControl) {
-        control = subControl.get(name);
-      }
-    }
-    if (control) {
-      console.log("isValid: ", control)
-      return control.valid || !control.touched;
-    } else {
-      return true;
-    }
-  }
+
+  // isValid(groupKey: string, name: string ): any {
+  //   let control = null;
+  //   if (this.rootForm) {
+  //     let subControl = this.rootForm.get(groupKey) as FormControl;
+  //     if (subControl) {
+  //       control = subControl.get(name);
+  //     }
+  //   }
+  //   if (control) {
+  //     console.log("isValid: ", control)
+  //     return control.valid || !control.touched;
+  //   } else {
+  //     return true;
+  //   }
+  // }
 
   private _setupObservables = (): void => {
     this.rootForm.valueChanges.pipe(takeUntil(this._destroy$)).subscribe(value => {
