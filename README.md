43374df1ddf9b6f2c12d9f75e55b1e422030e0ac
kytlam <kytl0m@gmail.com>
2021-12-04 11:16:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# devexpress-datagrid-secdyv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/devexpress-datagrid-secdyv)
---
+++ src/app/custom-async-validator.ts
@@ -0,0 +1,28 @@
+import {
+  AsyncValidatorFn,
+  ValidationErrors,
+  AbstractControl,
+} from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { mapTo, delay } from 'rxjs/operators';
+import { merge } from 'rxjs/observable/merge';
+import 'rxjs/add/observable/of';
+
+export function AsyncValidator(): AsyncValidatorFn {
+  return (
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
+    const test = Observable.of(null);
+    if (control.value) {
+      if (
+        control.value === 1 ||
+        control.value === 2 ||
+        control.value === '1' ||
+        control.value === '2'
+      ) {
+        return test.pipe(mapTo({ AlreadyExists: 'not valid' }), delay(1000));
+      }
+    }
+    return test;
+  };
+}
